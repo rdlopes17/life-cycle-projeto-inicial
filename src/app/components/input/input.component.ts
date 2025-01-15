bc145681013138a886d4ee0ffc98d260e45753b3
rdlopes17 <rauldlopes@gmail.com>
2025-01-15 08:25:07 -0300

    desenv-pt4 adicionando itens a lista criando servico

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-input',
@@ -8,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class InputComponent implements OnInit {
   valorItem!: string;
 
-  constructor() {}
+  constructor(private listaService: ListaDeCompraService) {}
 
   ngOnInit(): void {}
 
   adicionarItem() {
-    console.log('chamou o adicionarItem', this.valorItem);
+    this.listaService.adicionarItemNaLista(this.valorItem);
+    this.limparCampo;
+  }
+
+  limparCampo() {
+    this.valorItem = '';
   }
 }
--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -2,36 +2,51 @@ import { Item } from 'src/app/interfaces/iItem';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ListaDeCompraService {
-
   private listaDeCompra: Item[] = [
     {
-      "id": 1,
-      "nome": "Queijo prato",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
+      id: 1,
+      nome: 'Queijo prato',
+      data: 'Segunda-feira (31/10/2022) às 08:30',
+      comprado: false,
     },
     {
-      "id": 2,
-      "nome": "Leite integral",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
+      id: 2,
+      nome: 'Leite integral',
+      data: 'Segunda-feira (31/10/2022) às 08:30',
+      comprado: false,
     },
     {
-      "id": 3,
-      "nome": "Mamão papaia",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": true
+      id: 3,
+      nome: 'Mamão papaia',
+      data: 'Segunda-feira (31/10/2022) às 08:30',
+      comprado: true,
     },
-  ]
+  ];
 
   constructor() {
     console.log('Instanciando dependências necessárias para o serviço.');
   }
 
-  getListaDeCompra(){
+  getListaDeCompra() {
     return this.listaDeCompra;
+  }
+
+  criarItem(nomeDoItem: string) {
+    const id = this.listaDeCompra.length + 1;
+    const item: Item = {
+      id: id,
+      nome: nomeDoItem,
+      data: new Date().toLocaleString('pt-BR'),
+      comprado: false,
+    };
+    return item;
+  }
+
+  adicionarItemNaLista(nomeDoItem: string) {
+    const item = this.criarItem(nomeDoItem);
+    this.listaDeCompra.push(item);
   }
 }
