2f57ed9d780e9d59ea4dd7047d10ba8e81d04662
rdlopes17 <rauldlopes@gmail.com>
2025-01-20 08:37:02 -0300

    desenv-pt6 editando item usando simpleChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { ListaDeCompraService } from './service/lista-de-compra.service';
 export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
   listaDeCompra!: Array<Item>;
+  itemParaSerEditado!: Item;
 
   constructor(private listaService: ListaDeCompraService) {}
 
@@ -18,6 +19,6 @@ export class AppComponent implements OnInit {
   }
 
   editarItem(item: Item) {
-    console.log(item);
+    this.itemParaSerEditado = item;
   }
 }
