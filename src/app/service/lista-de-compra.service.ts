e356830a597dbec931717ebeaae2843fa8b6456e
rdlopes17 <rauldlopes@gmail.com>
2025-03-17 08:55:33 -0300

    desenv-pt8 usando a lista no localStorage

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -5,28 +5,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ListaDeCompraService {
-  private listaDeCompra: Item[] = [
-    {
-      id: 1,
-      nome: 'Queijo prato',
-      data: 'Segunda-feira (31/10/2022) às 08:30',
-      comprado: false,
-    },
-    {
-      id: 2,
-      nome: 'Leite integral',
-      data: 'Segunda-feira (31/10/2022) às 08:30',
-      comprado: false,
-    },
-    {
-      id: 3,
-      nome: 'Mamão papaia',
-      data: 'Segunda-feira (31/10/2022) às 08:30',
-      comprado: true,
-    },
-  ];
+  private listaDeCompra: Item[];
 
   constructor() {
+    this.listaDeCompra = JSON.parse(localStorage.getItem('itens') || '[]');
     console.log('Instanciando dependências necessárias para o serviço.');
   }
 
@@ -60,4 +42,5 @@ export class ListaDeCompraService {
     const id = ItemAntigo.id;
     this.listaDeCompra.splice(Number(id) - 1, 1, itemEditado);
   }
+
 }
